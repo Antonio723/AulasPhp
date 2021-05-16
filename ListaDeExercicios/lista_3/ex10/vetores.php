d191be3a7ba0192a1a4ae8d3af2b4e30724075aa
AntonioAiltonGoncalves <antonioailton723@gmail.com>
2021-05-16 11:01:14 -0300

    10

--- ListaDeExercicios/lista_3/ex10/vetores.php
+++ ListaDeExercicios/lista_3/ex10/vetores.php
@@ -6,9 +6,26 @@ $b = $_POST["vetorpesquisa"];
 $c = explode(" ",$a);
 $d = explode(" ",$b);
 
-function teste (array $vetorA, array $vetorB){   
-    $encontrar = false;
-    foreach
+function veotoresiguais (array $vetor, array $vetorcontido){   
+    $encotrado = false;
+    foreach($vetor as $valorVetor1){
+        foreach($vetorcontido as $valorVetor2){
+            if($valorVetor1==$valorVetor2){
+                $encotrado = true;
+                continue;
+            }
+        }
+    }
+    if(!$encotrado){
+        return false;
+    }
+    return true;
+}
+
+if(veotoresiguais($c,$d)){
+    echo "Sim, está contido";
+}else{
+    echo "Não, não está contido";
 }
 
 
